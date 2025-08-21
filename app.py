de7d1debbc659fd698f728a8b20f610a21be3b72
Qasimhassan65 <qasimhassan65@gmail.com>
2025-08-22 04:07:29 +0500

    Describe what you updated

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import threading
 
 
 from langchain.docstore.document import Document
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
